15e8591775d33134986b751c6fe14260008b513f
deeseert <gio.amms@gmail.com>
2019-03-24 21:18:12 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,4 @@
 class Artist < ActiveRecord::Base
+  has_many :songs #It says that an artist has mnany songs
+  has_many :genres, through: :songs #it also says that an artist has many genres through Song. So easy!!
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ActiveRecord::Base
-
+  belongs_to :artist #these two calls macros from AR
+  belongs_to :genre #these two calls macros from AR
 end
